6a122e07c5d5d55b7557032be088e44f23301285
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-12 17:45:52 +0530

    Feat(Open Routes): Define Registartion and login routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TenantController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+
+Route::middleware(['cors'])->group(function () {
+   Route::post('registerTenant', [TenantController::class, 'store']);
+   Route::post('register', [AuthController::class, 'registerUser']);
+   Route::post('login', [AuthController::class, 'login']);
+});
