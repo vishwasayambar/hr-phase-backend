201e86a1556899e9720ff047ebfdf1b7911f8deb
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-12 17:45:05 +0530

    Feat(Auth); Schedule command to clear expired tokens from tokens table

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('sanctum:prune-expired --hours=24')
+                    ->daily()
+                    ->sendOutputTo(storage_path('logs/command.log'));
+
