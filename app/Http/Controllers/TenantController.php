364ac43bcfef8ebf395de739467b97512ca70067
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-22 23:29:22 +0530

    pint Fices

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -7,6 +7,7 @@ use App\Models\Tenant;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class TenantController extends Controller
@@ -18,11 +19,12 @@ class TenantController extends Controller
         $tenant = Tenant::latest()->first();
         $accountId = $tenant ? 100000 + $tenant->id + 1 : 100001;
 
+        Log::info(print_r($request->all(), true));
         DB::transaction(function () use ($accountId, $tenant, $request){
            Tenant::query()->create([
                 'name' => ucwords($request->input('company_name')),
                 'email' => $request->input('email'),
-                'support_email' => fake()->optional()->safeEmail,
+                'support_email' => $request->input('email'),
                 'support_number' => $request->input('mobile_number'),
                 'plan' => $request->input('plan'),
                 'source' => $request->input('source'),
