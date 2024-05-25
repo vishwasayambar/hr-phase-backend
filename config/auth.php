2d4664ba60c1beee4b9f84325e9ed6bc0d7b6d2c
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-25 08:36:10 +0530

    Change guard web to api

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => env('AUTH_GUARD', 'api'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -36,10 +36,16 @@ return [
     */
 
     'guards' => [
-        'web' => [
+        'api' => [
             'driver' => 'session',
             'provider' => 'users',
+            'model' => App\Models\User::class,
         ],
+
+//        'web' => [
+//            'driver' => 'session',
+//            'provider' => 'users',
+//        ],
     ],
 
     /*
