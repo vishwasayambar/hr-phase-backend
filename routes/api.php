3690bf0378c525376189b164c0cd0fe7b4fc0f06
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-31 19:57:19 +0530

    Refactor routes

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TenantController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
-
-
 Route::middleware(['cors'])->group(function () {
-   Route::post('registerTenant', [TenantController::class, 'store']);
-   Route::post('register', [AuthController::class, 'registerUser']);
-   Route::post('login', [AuthController::class, 'login']);
+    Route::group([
+        'prefix' => "register",
+    ], function () {
+        Route::post('/', [TenantController::class, 'store']);
+        Route::post('/resendActivationEmail', [TenantController::class, 'resendActivationEmail']);
+    });
+    Route::prefix('auth')->group(function () {
+        Route::get('verifyToken', [AuthController::class, 'verifyToken']);
+        Route::post('accountActivate', [AuthController::class, 'accountActivate']);
+        Route::post('login', [AuthController::class, 'login']);
+    });
 });
 
-Route::prefix('auth')->middleware(['cors'])->group(function () {
-    Route::get('verifyToken', [AuthController::class, 'verifyToken']);
-    Route::post('accountActivate', [AuthController::class, 'accountActivate']);
-    Route::post('login', [AuthController::class, 'login']);
-});
