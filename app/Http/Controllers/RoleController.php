6a12d1a52e66579e2b32cfd0f3a3c00f15e357f8
Vishwa <vishwajitsayambar92@gmail.com>
2024-11-27 22:42:34 +0530

    Feat(Role): HP-6 Added Store Method in Role Controller

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreRoleRequest;
 use App\Models\Role;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cache;
 
 class RoleController
 {
+
+    public function store(StoreRoleRequest $request): \Spatie\Permission\Contracts\Role|\Spatie\Permission\Models\Role
+    {
+        return Role::create($request->validated());
+    }
+
     public function getEmployeeRoles(): Response
     {
         return response(Cache::rememberForever('employee_roles', fn () => Role::query()->get()));
